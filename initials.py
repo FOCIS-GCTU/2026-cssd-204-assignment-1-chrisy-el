22c2119843f0ecb0d98f0f066e2aad18554f0b9d
maniel <manuelpepr99@icloud.com>
2026-05-31 20:39:46 +0000

    Adding initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+#File: initials.py
+#Description: Print out my initials
+#Assignment Number: 1
+#
+#Name: Chrislyn Dadia Selassie
+#STUDENT ID: 2425403790
+#Email: 2425403790@live.gctu.edu.gh
+#Grader: Augustus
+#
+#On my honor, Chrislyn Dadia, this programming assignment is my own work 
+#and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print my initials in large letters
 
+    print()
+    print("...CDS")
+    print()
+    
+    print(".......CCCCCCCC........DDDDDDDD..............SSSSSSSSSS.....")
+    print(".....CCCCCCCCCC........DDDDDDDDDD...........SSSSSSSSSSS.....")
+    print("...CCC.................DD.......DDD........SSSS.............")
+    print("...CC..................DD........DD........SSSS.............")
+    print("...CC..................DD........DD..........SSSSS..........")
+    print("...CC..................DD........DD............SSSSSS.......")
+    print("...CC..................DD........DD................SSSS.....")
+    print("...CCC.................DD.......DDD................SSSS.....")
+    print(".....CCCCCCCCCC...**...DDDDDDDDDD.....**...SSSSSSSSSSS....**")
+    print(".......CCCCCCCC...**...DDDDDDDD.......**...SSSSSSSSSS.....**")
+
+    print()
 
 
 main()
+
+
